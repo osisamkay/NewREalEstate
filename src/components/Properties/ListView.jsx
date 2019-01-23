939f0fb4d97182f4f6652ecc17f356e31c299723
osisamkay <osisami.oj@gmail.com>
2019-01-23 11:47:13 +0100

    Modified real estate

--- src/components/Properties/ListView.jsx
+++ src/components/Properties/ListView.jsx
@@ -42,20 +42,68 @@ const Liststyle = styled.div`
     left: 37%;
   }
   .viewleft{
+    box-shadow:0px 3px 6px #DADADB;
     .carousel .slide img {
       width: 100%;
       vertical-align: top;
       border: 0;
       object-fit: cover;
       height: 180px;
-  }
+      @media (min-width:768px){
+        height: 300px;
+      }
+    }
+    .thumbs-wrapper.axis-vertical {
+      text-align: center;
+    }
+    h2{
+      color:#28456F;
+      text-align:center;
+      text-transform: capitalize;
+      font-size:25px;
+    }
+    h4{
+      color:#28456F;
+      text-align:center;
+      font-size:25px;
+      margin:0px;
+      font-weight:400;
+    }
+    .agent{
+      padding-bottom:20px;
+
+    } 
+
   }
 `;
 
 const Info = styled.div`
-  display:grid;
-  grid-template-columns:repeat(2,1fr);
-  justify-items: center;
+  width:90%;
+  margin:0px auto;
+
+  h1,h2,h3,h4,h5,h6{
+    margin:0px;
+    display:inline;
+  }
+  div{
+    padding-bottom:20px;
+  }
+  h6{
+    display: inline;
+    font-size: 15px;
+  }
+  
+  img{
+    width:17px;
+  }
+ @media (min-width:768px){
+   display:grid;
+   grid-template-columns:1fr 1fr;
+
+   .info {
+    margin-left: 24%;
+  }
+ }
 `;
 
 const Listgroup = styled.div`
@@ -86,7 +134,7 @@ class ListView extends Component {
       url: `https://api.airtable.com/v0/apprAJrG1euRf2tmF/Listings/${id}`,
       headers: { Authorization: `Bearer keyRMRWZ0xrBXA8Yv` },
     }).then(({ data }) => {
-      console.log(data.fields.Tag)
+      console.log(data)
       this.setState({
         list: data,
         ready: 'loaded',
@@ -125,18 +173,44 @@ class ListView extends Component {
                       <img src={list.fields.icon[4].url} />
                     </div>
                   </Carousel>
+                  <h2>{list.fields.Tag}</h2>
+                  <Info>
 
+                    <div className="size info">
+                      <img src={require("../../../assets/Path 30.png" )}alt="location"/>
+                      <h6>{list.fields.Name}</h6>
+                    </div>
+                    <div className="size info">
+                      <img src={require("../../../assets/width (1).png")} alt="area" />
+                      <h6>{list.fields.Area}</h6>
+                    </div>
+                    <div className="bed info">
+                      <img src={require('../../../assets/bed.png')} alt="bed" />
+                      <h6>{list.fields.Bedrooms}</h6>
+                    </div>
+                    <div className="bath info">
+                      <img src={require('../../../assets/bathtub-with-opened-shower.png')} alt="bath" />
+                      <h6>{list.fields.Bathrooms}</h6>
+                    </div>
+                    <div className="status info">
+                      <h5>Status: </h5>
+                      <h6>{list.fields.Status}</h6>
+                    </div>
+                    <div className="id info">
+                      <h5>id: </h5>
+                      <h6>{list.id}</h6>
+                    </div>
+                  </Info>
+
+                  <h4>Get it for</h4>
+                  <h2>{list.fields.Asking}</h2>
+                  <h4 className='agent'>Contact the agent if interested.</h4>
                 </div>
                 <div className="viewright">
-                  <h2>{list.fields.Tag}</h2>
+                  
                   <h4>{list.fields.Asking}</h4>
                   <h5>{list.fields.Summary}</h5>
-                  <Info>
-                    <h6>Bedrooms: {list.fields.Bedrooms}</h6>
-                    <h6>Bathrooms: {list.fields.Bathrooms}</h6>
-                    <h6>Area: {list.fields.Area}</h6>
-                    <h6>Status: {list.fields.Status}</h6>
-                  </Info>
+                  
                   <div className="btn">
                     <Link to="/Listings">
                       <input type="button" value="Return" />
