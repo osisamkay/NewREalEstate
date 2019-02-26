9dd0134947efa597cc6cf162e11e509b8b25d738
osisamkay <osisami.oj@gmail.com>
2019-02-26 01:13:02 +0100

    implimented search filter on home page

--- src/components/Homepage/SectionACard.jsx
+++ src/components/Homepage/SectionACard.jsx
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router';
+import React, { Component, Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
+import ListCard from './../Properties/ListCard';
+import Modal from 'react-responsive-modal';
+import './modal.css';
 
 const CardStyle = styled.form`
   .stylegroup{
@@ -73,25 +76,35 @@ const CardStyle = styled.form`
 
 
 
+
 export default class SectionACard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       value: { min: 0, max: 100000000 },
-      search:" ",
+      search: " ",
       lists: [],
+      open: false,
     };
   }
-  LocationChange=(e)=>{
+  LocationChange = (e) => {
     this.setState({
       search: e.target.value
     })
   };
-  
-  
-  submit(event) {
-    event.preventDefault();
+
+  onOpenModal = (e) => {
+    e.preventDefault();
+    this.setState({ open: true });
+  };
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+
+  componentDidMount() {
+   
     axios({
       method: 'get',
       url: `https://api.airtable.com/v0/apprAJrG1euRf2tmF/Listings`,
@@ -106,64 +119,82 @@ export default class SectionACard extends Component {
     })
   }
   render() {
-    const {lists, search} = this.state;
-   const filtered = lists.filter(list => {
+    const { lists, search, open } = this.state;
+    const filtered = lists.filter(list => {
       return list.fields.Name.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
         list.fields.Bedrooms.toLowerCase().indexOf(search.toLowerCase()) !== -1 ||
         list.fields.Tag.toLowerCase().indexOf(search.toLowerCase()) !== -1
     });
     return (
       <div>
-      <CardStyle onSubmit={this.submit.bind(this)}>
-        <div className="stylegroup">
-          <div className="Location filter">
-          <input type="search" name="Location" placeholder='Location' onChange={this.LocationChange}  />
+        <CardStyle onSubmit={this.onOpenModal} >
+          <div className="stylegroup">
+            <div className="Location filter">
+              <input type="search" name="Location" required placeholder='Location' onChange={this.LocationChange} />
+            </div>
+            <div className="Category filter">
+              <select name="Category-type" className="app-select" required>
+                <option data-display="Location Type">Category</option>
+                <option value="1">Category  1</option>
+                <option value="2">Category  2</option>
+                <option value="3">Category  3</option>
+              </select>
+            </div>
+            <div className="Bedrooms filter">
+              <select name="Bedrooms-type" className="app-select" required>
+                <option data-display="Bedrooms Type">Bedroom</option>
+                <option value="1">Bedrooms  1</option>
+                <option value="2">Bedrooms  2</option>
+                <option value="3">Bedrooms  3</option>
+              </select>
+            </div>
+            <div className="AreaRange filter">
+              <select name="Location-type" className="app-select" required>
+                <option data-display="Area Range">Area Range</option>
+                <option value="1">Area Range 1</option>
+                <option value="2">Area Range  2</option>
+                <option value="3">Area Range  3</option>
+              </select>
+            </div>
+            <div className="range1 filter">
+              <h3>₦100,000</h3>
+              <input id="typeinp" type="range" min="0" max="100,000,000" defaultValue="3" step="1" />
+              <h3>100,000,000</h3>
+            </div>
+            <div className="search filter">
+              <input type="submit" value="Search"   />
+            </div>
           </div>
-          <div className="Category filter">
-            <select name="Category-type" className="app-select" required>
-              <option data-display="Location Type">Category</option>
-              <option value="1">Category  1</option>
-              <option value="2">Category  2</option>
-              <option value="3">Category  3</option>
-            </select>
+          <div className="range2 filter">
+            <h5>₦100,000</h5>
+            <input id="typeinp" type="range" min="0" max="100,000,000" value={this.state.values} step="1" />
+            <h5>₦100,000,000</h5>
           </div>
-          <div className="Bedrooms filter">
-            <select name="Bedrooms-type" className="app-select" required>
-              <option data-display="Bedrooms Type">Bedroom</option>
-              <option value="1">Bedrooms  1</option>
-              <option value="2">Bedrooms  2</option>
-              <option value="3">Bedrooms  3</option>
-            </select>
-          </div>
-          <div className="AreaRange filter">
-            <select name="Location-type" className="app-select" required>
-              <option data-display="Area Range">Area Range</option>
-              <option value="1">Area Range 1</option>
-              <option value="2">Area Range  2</option>
-              <option value="3">Area Range  3</option>
-            </select>
-          </div>
-          <div className="range1 filter">
-            <h3>₦100,000</h3>
-            <input id="typeinp" type="range" min="0" max="100,000,000" defaultValue="3" step="1" />
-            <h3>100,000,000</h3>
-          </div>
-          <div className="search filter">
-            <input type="submit" value="Search" />
-          </div>
+        </CardStyle>
+        <Modal open={open} onClose={this.onCloseModal} center>
+        <div className="modals">
+          {filtered.map(list => (
+            <div key={list.id}>
+              <Link to={`/Listview/${list.id}`}>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ListCard
+                    image={list.fields.icon ? list.fields.icon[0].url : ''}
+                    Price={list.fields.Asking}
+                    location={list.fields.Name}
+                    bath={list.fields.Bathrooms}
+                    bed={list.fields.Bedrooms}
+                    area={list.fields.Area} >
+                    <div className="child1">
+                      <h4>{list.fields.Tag}</h4>
+                      <div className='button'><button>View</button></div>
+                    </div>
+                  </ListCard>
+                </Suspense>
+              </Link>
+            </div>
+          ))}
         </div>
-        <div className="range2 filter">
-          <h5>₦100,000</h5>
-          <input id="typeinp" type="range" min="0" max="100,000,000" value={this.state.values} step="1" />
-          <h5>₦100,000,000</h5>
-        </div>
-      </CardStyle>
-      {filtered.length > 0 &&
-          <Redirect to={{
-            pathname: '/Result',
-            state: { results: filtered }
-          }}/>
-        }
+        </Modal>
       </div>
     )
   }
